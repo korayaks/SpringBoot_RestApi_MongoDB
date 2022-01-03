b186f8329c564ec81cb03e22ca6c5727bb726328
Koray Aksoy <burakkorayaksoy@hotmail.com>
2022-01-03 18:20:10 +0300

    initial

---
+++ src/main/java/com/restapi_mongodb/springboot_restapi_mongodb/SpringBootRestApiMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.restapi_mongodb.springboot_restapi_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiMongoDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestApiMongoDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/restapi_mongodb/springboot_restapi_mongodb/SpringBootRestApiMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi_mongodb.springboot_restapi_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiMongoDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
